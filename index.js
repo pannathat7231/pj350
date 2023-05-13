0e8a7ed120d29bceebeb530bf86052d67c6c3591
pannathat7231 <132830442+pannathat7231@users.noreply.github.com>
2023-05-13 11:28:03 +0700

    index.js

--- index.js
+++ index.js
@@ -1,32 +1,139 @@
-const express = require('express');
-const cors = require('cors');
+var express = require('express')
+var cors = require('cors')
 const mysql = require('mysql2');
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+var app = express()
+app.use(cors())
+app.use(express.json())
 
-app.use(cors());
+app.get('/user', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM `user`',
+        function (err, results, fields) {
+            res.json(results);
+        }
+    );
+})
 
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+app.get('/user/:id', function (req, res, next) {
+    const uid = req.params.uid;
+    connection.query(
+        'SELECT * FROM user WHERE uid = ?',
+        [uid],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+})
 
-app.get('/', (req, res) => {
-  res.send('Hello there');
-});
+app.post('/user', function (req, res, next) {
+    connection.query(
+        'INSERT INTO `user`( `username`, `password`,`email`, `avatar`) VALUES (?, ?, ?)',
+        [req.body.username, req.body.password, req.body.email, req.body.avatar],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+})
 
-app.get('/tourd', (req, res) => {
-  connection.query('SELECT * FROM tourd', function(err, results, fields) {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+app.put('/user', function (req, res, next) {
+    connection.query(
+        'UPDATE `user` SET `username`= ?, `password`= ?, `email`= ? WHERE uid = ?',
+        [req.body.username, req.body.password, req.body.email, req.body.uid],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+})
 
-app.get('/user', (req, res) => {
-  connection.query('SELECT * FROM user', function(err, results, fields) {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+app.delete('/user', function (req, res, next) {
+    connection.query(
+        'DELETE FROM users WHERE uid = ?',
+        [req.body.uid],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+})
+////////////////////////////////////////////////////////////////////////////////////////////
+//orders
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.get('/tourd', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM `tourd`',
+        function (err, results, fields) {
+            res.json(results);
+        }
+    );
+})
+
+app.get('/tourd/:id', function (req, res, next) {
+    const id = req.params.order_ID;
+    connection.query(
+        'SELECT * FROM tourd WHERE id = ?',
+        [id],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+})
+
+app.post('/tourd', function (req, res, next) {
+    connection.query(
+        'INSERT INTO `tourd`(`tourname`,`hostname`,`prizepool`,`contract`) VALUES (?,?,?,?)',
+        [req.body.tourname, req.body.hostname, req.body.prizepool, req.body.contract],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+})
+
+app.put('/tourd', function (req, res, next) {
+    connection.query(
+        'UPDATE tourd SET `tourname`= ?,`hostname`= ?,`prizepool`= ? ,`contract`= ? WHERE id = ?',
+        [req.body.tourname, req.body.hostname, req.body.prizepool, req.body.contract,req.body.order_ID],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+})
+
+app.delete('/tourd', function (req, res, next) {
+    connection.query(
+        'DELETE FROM tourd WHERE id = ?',
+        [req.body.id],
+        function (err, results) {
+            res.json(results);
+        }
+    );
+
+})
+
+app.post('/login', function (req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    connection.query(
+      'SELECT * FROM user WHERE username = ? AND password = ?',
+      [username, password],
+      function(err, results) {
+        if (err) {
+          res.send({ err : err})
+        }
+  
+        if (results.length > 0) {
+        res.send({ results})
+          
+        }else{
+        res.send({ message: "Wrong username or password"})
+        }
+
+    })
+})
+
+
+app.listen(3000, function () {
+    console.log(' web server listening on port 3000')
+})
