2d0c72e262bf611637e18372d8f57cdb0221c491
pannathat7231 <132830442+pannathat7231@users.noreply.github.com>
2023-05-13 15:17:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.put('/updateuser', function (req, res, next) {
 
 app.delete('/deleteuser', function (req, res, next) {
     connection.query(
-        'DELETE FROM users WHERE uid = ?',
+        'DELETE FROM user WHERE uid = ?',
         [req.body.uid],
         function (err, results) {
             res.json(results);
